a5b2c7b7ac5020b21d1cbd9b3af82797baf4455a
Anton Coding <43090912+AntonIT99@users.noreply.github.com>
2023-04-25 19:16:26 +0200

    changed hitbox of ratte

--- src/main/java/com/wolffsmod/entity/prototype/EntityRatte.java
+++ src/main/java/com/wolffsmod/entity/prototype/EntityRatte.java
@@ -9,14 +9,15 @@ public class EntityRatte extends EntityFlanVehicleNPC
     public EntityRatte(World w)
     {
         super(w);
-        setSize(10F, 10F);
+        setSize(19F, 18F);
     }
     
     @Override
     public void setupConfig()
     {
-        setTurnSpeed(0.3F);
+        setTurnSpeed(0.1F);
         setDriver("8 248 0 -360 360 -10 40");
+        setDriverAimSpeed("0.25 0.15 0");
         setRotatedDriverOffset("6 0 -12");
         setPassenger("1 216 120 -30 core -12 12 -24 24 128mmGun Mantle");
         setPassenger("2 -386 200 -114 core -360 360 -15 20 128mmGun Left");
@@ -24,7 +25,7 @@ public class EntityRatte extends EntityFlanVehicleNPC
         setPassenger("4 -194 178 -124 core -360 360 -30 90 FlakRatte FlakLeft");
         setPassenger("5 -194 178 124 core -360 360 -30 90 FlakRatte FlakRight");
         addShootPointPrimary("89.6 215.8 -47 turret");
-        addShootPointSecondary("89.6 215.8 47 turret");
+        addShootPointPrimary("89.6 215.8 47 turret");
         addGunOrigin("1 216 118 -30");
         addGunOrigin("2 -386 185 -114");
         addGunOrigin("3 -386 185 114");
