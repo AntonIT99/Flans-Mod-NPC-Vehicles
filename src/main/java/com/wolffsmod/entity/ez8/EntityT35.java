528ba1a1360bbb03247ee178ca40f4c71f5abecb
Anton Coding <43090912+AntonIT99@users.noreply.github.com>
2023-09-02 18:54:51 +0200

    Fixed T-35

--- src/main/java/com/wolffsmod/entity/ez8/EntityT35.java
+++ src/main/java/com/wolffsmod/entity/ez8/EntityT35.java
@@ -19,16 +19,14 @@ public class EntityT35 extends EntityFlanVehicleNPC
         setDriver("0 32 0 -360 360 -5 25");
         setRotatedDriverOffset("-4 0 8");
 
-        //TODO: fix multi turrets tank guns
-        /*setPassenger("1 26 17 8 nose -30 135 -5 28 vg_45mm_m1934 Nose");
-        setPassenger("2 -26 17 -8 nose 145 310 -5 28 vg_45mm_m1934 Nose2");
-        setPassenger("3 22 14 -11 nose -135 20 -10 30 vg_7.62mm_lmg Nose3");
-        setPassenger("4 -22 14 11 nose 45 200 -10 30 vg_7.62mm_lmg Nose4");*/
-
-        setPassenger("1 26 17 8 nose 0 0 -5 28 vg_45mm_m1934 Nose");
-        setPassenger("2 -26 17 -8 nose 0 0 -5 28 vg_45mm_m1934 Nose2");
-        setPassenger("3 22 14 -11 nose 0 0 -10 30 vg_7.62mm_lmg Nose3");
-        setPassenger("4 -22 14 11 nose 0 0 -10 30 vg_7.62mm_lmg Nose4");
+        setPassenger("1 26 17 8 nose -30 135 -5 15 vg_45mm_m1934 Nose");
+        setPassenger("2 -26 17 -8 nose 145 310 -5 15 vg_45mm_m1934 Nose2");
+        setPassenger("3 22 14 -11 nose -135 20 -10 10 vg_7.62mm_lmg Nose3");
+        setPassenger("4 -22 14 11 nose 45 200 -10 10 vg_7.62mm_lmg Nose4");
+        setPassengerOffsetYaw(1, 0F);
+        setPassengerOffsetYaw(2, 180F);
+        setPassengerOffsetYaw(3, 0F);
+        setPassengerOffsetYaw(4, 180F);
 
         addShootPointPrimary("30 38 0 turret");
         addShootPointSecondary("23 35 10 turret");
